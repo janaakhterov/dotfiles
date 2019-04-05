b1ada8daf727ccb60c500a7e3de5207090bb475e
Daniel Akhterov <akhterovd@gmail.com>
2019-04-04 18:12:36 -0700

    Move zsh files outside zsh directory

--- .config/scripts/install.sh
+++ .config/scripts/install.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Set this just in case otherwise nix might not install 
+sysctl kernel.unprivileged_userns_clone=1
+
 # Install Nix package manager
-bash <(curl https://nixos.org/nix/install)
+curl https://nixos.org/nix/install | sh
 . $HOME/.nix-profile/etc/profile.d/nix.sh
+
+# Exa
+nix-env --install exa
+
+# Yadm
+nix-env --install yadm
+
+# Initialize dotfiles
+cd $HOME
+yadm clone https://github.com/danielakhterov/.dotfiles.git
+yadm submodule update --init
