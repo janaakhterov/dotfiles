618945708e5dbe193c052e18f207b402df59246a
Daniel Akhterov <daniel@launchbadge.com>
2019-05-14 21:43:23 -0700

    Update installer

--- .config/scripts/install.sh
+++ .config/scripts/install.sh
@@ -1,22 +1,41 @@
 #!/bin/sh
+cd $HOME
 
 # Set this just in case otherwise nix might not install 
-sysctl kernel.unprivileged_userns_clone=1
+sudo sysctl kernel.unprivileged_userns_clone=1
 
 # Install Nix package manager
 curl https://nixos.org/nix/install | sh
 . $HOME/.nix-profile/etc/profile.d/nix.sh
 
-# Exa
 nix-env --install exa
-
-# Yadm
 nix-env --install yadm
+nix-env --install fish
+nix-env --install go
+nix-env --install fzf
+nix-env --install diff-so-fancy
+
+# Diff-so-fancy
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
 # Initialize dotfiles
-cd $HOME
-yadm clone https://github.com/danielakhterov/.dotfiles.git
-yadm submodule update --init
+yadm clone --recurse-submodules -j8 https://github.com/danielakhterov/.dotfiles.git
 
-# Install ZPlugin
+# ZPlugin
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+
+# GHQ
+go get github.com/motemen/ghq
+
+# Fisher
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish && fisher
+
+# Change shell to fish
+chsh -s /usr/bin/fish
+
+# i3lock-fancy
+git clone https://github.com/meskarune/i3lock-fancy
+cd $HOME/i3lock-fancy
+sudo make install
+cd $HOME
+rm -rf i3lock-fancy
