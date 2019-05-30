aed22741b3b13456665dd5cd7d87f81d1f27c036
Daniel Akhterov <akhterovd@gmail.com>
2019-05-29 21:27:13 -0700

    Use home-manager

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.packages = [
+    pkgs.alacritty
+    pkgs.exa
+    pkgs.firefox
+    pkgs.fish
+    pkgs.fzf
+    pkgs.ghq
+    pkgs.go
+    pkgs.hack-font
+    pkgs.i3
+    pkgs.nitrogen
+    pkgs.polybar
+    pkgs.yadm
+  ];
+
+  programs.firefox.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "Daniel Akhterov";
+    userEmail = "akhterovd@gmail.com";
+  };
+
+  xsession.enable = true;
+  xsession.windowManager.command = "i3";
+}
