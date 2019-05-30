32db115ff1f04fbce27f00751509663917d1defd
Daniel Akhterov <akhterovd@gmail.com>
2019-05-29 17:13:45 -0700

    Nixos configs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-#!/usr/bin/env sh
+#!/bin/sh
 
 # Terminate already running bar instances
-killall -q polybar
+pkill polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-#polybar example &
-
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar top -c ~/.config/polybar/config & done
+# Launch top bar
+polybar top
 
 echo "Bars launched..."
--- .config/scripts/install.sh
+++ .config/scripts/install.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-cd $HOME
-
 # Set this just in case otherwise nix might not install 
-sudo sysctl kernel.unprivileged_userns_clone=1
+# sudo sysctl kernel.unprivileged_userns_clone=1
 
 # Install Nix package manager
-curl https://nixos.org/nix/install | sh
-. $HOME/.nix-profile/etc/profile.d/nix.sh
+# curl https://nixos.org/nix/install | sh
+# . $HOME/.nix-profile/etc/profile.d/nix.sh
 
 nix-env --install exa
 nix-env --install yadm
@@ -16,6 +14,9 @@ nix-env --install fzf
 nix-env --install diff-so-fancy
 nix-env --install nitrogen
 nix-env --install polybar
+nix-env --install firefox
+nix-env --install i3lock-fancy-unstable
+nix-env --install ghq
 
 # Diff-so-fancy
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
@@ -26,18 +27,5 @@ yadm clone --recurse-submodules -j8 https://github.com/danielakhterov/.dotfiles.
 # ZPlugin
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 
-# GHQ
-go get github.com/motemen/ghq
-
 # Fisher
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish && fish --command fisher
-
-# Change shell to fish
-chsh -s /usr/bin/fish
-
-# i3lock-fancy
-git clone https://github.com/meskarune/i3lock-fancy
-cd $HOME/i3lock-fancy
-sudo make install
-cd $HOME
-rm -rf i3lock-fancy
