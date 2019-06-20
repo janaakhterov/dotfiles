2d26398911dada13e0667946391f0905da8a4040
Daniel Akhterov <akhterovd@gmail.com>
2019-06-19 18:08:44 -0700

    Add packages

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -7,38 +7,53 @@ in
 {
   home.packages = [
     (pkgs.polybar.override { i3Support = true; pulseSupport = true; mpdSupport = true; })
+    pkgs._1password
     pkgs.alacritty
     pkgs.alsaUtils
     pkgs.androidStudioPackages.canary
     pkgs.bat
     pkgs.binutils
     pkgs.cargo-edit
-    # pkgs.discord
+    pkgs.ctags
+    pkgs.discord
     pkgs.dunst
     pkgs.exa
     pkgs.feh
     pkgs.firefox
     pkgs.fish
+    pkgs.font-awesome
+    pkgs.fuse
     pkgs.fzf
     pkgs.gcc
     pkgs.ghq
     pkgs.gitAndTools.diff-so-fancy
+    pkgs.gitlab-runner
     pkgs.go
+    pkgs.gnumake
     pkgs.hack-font
     pkgs.htop
     pkgs.i3
     pkgs.i3lock-fancy
     pkgs.jetbrains.idea-community
+    pkgs.jq
     pkgs.libnotify
     pkgs.maven
+    pkgs.mpv
+    pkgs.neovim
+    pkgs.ntfs3g
     pkgs.openjdk11
     pkgs.pulseaudio
+    pkgs.python
+    pkgs.python3
     pkgs.rofi-unwrapped
     pkgs.rustup
     pkgs.shutter
+    pkgs.slack
+    pkgs.spotify
     pkgs.steam
     pkgs.vim
     pkgs.xautolock
+    pkgs.xclip
     pkgs.yadm
     pkgs.youtube-dl
   ];
