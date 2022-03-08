05b042c225a9a790e132c9199ee355ed54152642
Daniel Akhterov <daniel@launchbadge.com>
2022-03-08 13:00:50 -0800

    feat: update i3 and nvim to use workman

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,5 +1,62 @@
 local options = { noremap = true }
 
+vim.cmd([[
+    noremap q q
+    noremap d w
+    noremap r e
+    noremap w r
+    noremap b t
+    noremap j y
+    noremap f u
+    noremap u i
+    noremap p o
+    noremap ; p
+    noremap a a
+    noremap s s
+    noremap h d
+    noremap t f
+    noremap g g
+    noremap y h
+    noremap n j
+    noremap e k
+    noremap o l
+    noremap i ;
+    noremap z z
+    noremap x x
+    noremap m c
+    noremap c v
+    noremap v b
+    noremap k n
+    noremap l m
+    noremap Q Q
+    noremap D W
+    noremap R E
+    noremap W R
+    noremap B T
+    noremap J Y
+    noremap F U
+    noremap U I
+    noremap P O
+    noremap : P
+    noremap A A
+    noremap S S
+    noremap H D
+    noremap T F
+    noremap G G
+    noremap Y H
+    noremap N J
+    noremap E K
+    noremap O L
+    noremap I :
+    noremap Z Z
+    noremap X X
+    noremap M C
+    noremap C V
+    noremap V B
+    noremap K N
+    noremap L M 
+]])
+
 vim.api.nvim_set_keymap("n", "<leader>g", [[<Cmd> lua require('telescope.builtin').git_status(require('telescope.themes').get_dropdown())<CR>]], options)
 vim.api.nvim_set_keymap("n", "<leader>o", [[<Cmd> lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown())<CR>]], options)
 vim.api.nvim_set_keymap("n", "<leader>p", [[<Cmd> lua require('telescope.builtin').oldfiles(require('telescope.themes').get_dropdown())<CR>]], options)
